fb9aab3f1a91aa9cf22e2abe23436f0c8c944fab
Otávio Santiago <otaviosatago@gmail.com>
2025-01-22 16:43:20 -0300

    mudei a porta para 8080

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post('/pesquisa', (req,res)=>{
 })
 
 // outros
-const PORT = 9090
+const PORT = 8080
 app.listen(PORT,()=>{
     console.log(`Servidor rodando em: http://localhost:${PORT}`)
 })
