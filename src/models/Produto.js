dba8445048bde4ba63e2b814c5e7dd4e7a1780f0
Otávio Santiago <otaviosatago@gmail.com>
2025-01-27 21:31:01 -0300

    mudei a estrutura da tabela produtos, de decimal para decimal(10,2)

--- src/models/Produto.js
+++ src/models/Produto.js
@@ -8,7 +8,7 @@ const Produto = sequelize.define('produtos',{
         allowNull:false
     },
     preco:{
-        type:DataTypes.DECIMAL,
+        type:DataTypes.DECIMAL(10,2),
         allowNull:false
     },
     path_foto:{
