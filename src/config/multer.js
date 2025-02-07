130b745d59a299cbac24176cc2cca3eb00b21c33
Otávio Santiago <otaviosatago@gmail.com>
2025-02-07 16:18:46 -0300

    Mudei o sistema de uploads

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,11 +1,14 @@
 const path = require('path')
 const multer = require('multer')
-
+const fs = require('fs')
 
 // config multer
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
         var pathFoto = path.join(__dirname,'../public/produtos')
+        if(!fs.existsSync(pathFoto)) {
+            fs.mkdirSync(pathFoto)
+        }
         cb(null,pathFoto)
     },
     filename:(req,file,cb)=>{
